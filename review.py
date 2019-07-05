bc7d7ce34903fbc9fc882aa10a4b96d962f4964e
nineone kuo <guo3325303@gmail.com>
2019-07-05 15:47:59 +0800

    'select'

--- review.py
+++ review.py
@@ -6,4 +6,18 @@ with open ('reviews.txt', 'r')as f:
 		count +=1 
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完了，共有', len(data), '筆資料')
+
+sum_len=0
+for d in data:
+	sum_len= sum_len + len(d)
+
+
+print('平均是', sum_len/len(data))
+
+new= []
+for d in data:
+	if len(d) < 100 :
+		new.append(d)
+print ('總夠有',len(new),'比留言長度小於100')
+print (new[0])
